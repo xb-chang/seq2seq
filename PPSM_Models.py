079c6e2b209c014832456eaf3e6aea036ac5f31a
xb-chang <littlesoliderchang@gmail.com>
2021-09-01 15:57:26 +0800

    update

--- PPSM_Models.py
+++ PPSM_Models.py
@@ -3,11 +3,14 @@ Packed padded sequences & Masking.
 '''
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+import random
 
 import pdb
 
 '''
-Encoder 
+Encoder: Packed padded sequences applied here.
 The changes here all within the forward method. It now accepts: 
 1. the lengths of the source sentences;
 2. the sentences themselves.
@@ -85,3 +88,264 @@ class Encoder(nn.Module):
         #hidden = [batch size, dec hid dim]
         
         return outputs, hidden
+
+'''
+Attention module: Masking is applied here.
+
+Using MASKING to force the attention on non-padding elements ONLY;
+The 'forward' method now takes a 'mask', which is a  [batch size, source sentence length] tensor:
+1: non padding element; 0: padding element; e.g.,
+["hello", "how", "are", "you", "?", <pad>, <pad>], then the mask would be [1, 1, 1, 1, 1, 0, 0].
+
+The mask is applied when the attention has been calculated, but before the attention softmax.
+It is applied using 'masked_fill'. Specifically, the padding attention elements will be set to 
+a relatively small number (-1e10) before softmax. So, the corresponding SOFTMAXed values will be very close
+to zeros.
+'''
+class Attention(nn.Module):
+    def __init__(self, enc_hid_dim, dec_hid_dim):
+        super().__init__()
+        
+        self.attn = nn.Linear((enc_hid_dim * 2) + dec_hid_dim, dec_hid_dim)
+        self.v = nn.Linear(dec_hid_dim, 1, bias = False)
+        
+    def forward(self, hidden, encoder_outputs, mask):
+        # hidden = [batch size, dec hid dim]
+        # encoder_outputs = [src len, batch size, enc hid dim * 2]
+        # mask = [batch size, src len]
+
+        batch_size = encoder_outputs.shape[1]
+        src_len = encoder_outputs.shape[0]
+
+        #repeat decoder hidden state src_len times
+        hidden = hidden.unsqueeze(1).repeat(1, src_len, 1)
+
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+        #hidden = [batch size, src len, dec hid dim]
+        #encoder_outputs = [batch size, src len, enc hid dim * 2]
+
+        energy = torch.tanh(self.attn(torch.cat((hidden, encoder_outputs), dim = 2))) 
+        #energy = [batch size, src len, dec hid dim]
+
+        attention = self.v(energy).squeeze(2)
+        #attention = [batch size, src len]
+        
+        # TRICK:
+        # MASKING: Fill before softmax
+        attention = attention.masked_fill(mask == 0, -1e10)
+        
+        return F.softmax(attention, dim = 1)
+
+
+'''
+Decoder: a few small changes
+It needs to accept a mask over the source sentence and pass it to the attention module.
+The attention tensor is returned for illustration.
+'''
+class Decoder(nn.Module):
+    def __init__(self, output_dim, emb_dim, enc_hid_dim, dec_hid_dim, dropout, attention):
+        super().__init__()
+
+        self.output_dim = output_dim
+        self.attention = attention
+        
+        self.embedding = nn.Embedding(output_dim, emb_dim)
+        
+        self.rnn = nn.GRU((enc_hid_dim * 2) + emb_dim, dec_hid_dim)
+        
+        self.fc_out = nn.Linear((enc_hid_dim * 2) + dec_hid_dim + emb_dim, output_dim)
+        
+        self.dropout = nn.Dropout(dropout)
+    
+    def forward(self, input, hidden, encoder_outputs, mask):
+        #input = [batch size]
+        #hidden = [batch size, dec hid dim]
+        #encoder_outputs = [src len, batch size, enc hid dim * 2] (zero padded back to 'src len')
+        #mask = [batch size, src len]
+
+        input = input.unsqueeze(0)
+        #input = [1, batch size]
+        
+        embedded = self.dropout(self.embedding(input))
+        #embedded = [1, batch size, emb dim]
+
+        # masking on attention
+        a = self.attention(hidden, encoder_outputs, mask)
+        #a = [batch size, src len]
+        
+        a = a.unsqueeze(1)
+        #a = [batch size, 1, src len]
+        
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+        #encoder_outputs = [batch size, src len, enc hid dim * 2]
+
+        weighted = torch.bmm(a, encoder_outputs)
+        #weighted = [batch size, 1, enc hid dim * 2]
+        
+        weighted = weighted.permute(1, 0, 2)
+        #weighted = [1, batch size, enc hid dim * 2]
+
+        rnn_input = torch.cat((embedded, weighted), dim = 2)
+        #rnn_input = [1, batch size, (enc hid dim * 2) + emb dim]
+            
+        output, hidden = self.rnn(rnn_input, hidden.unsqueeze(0))
+        #output = [seq len, batch size, dec hid dim * n directions]
+        #hidden = [n layers * n directions, batch size, dec hid dim]
+        
+        #seq len, n layers and n directions will always be 1 in this decoder, therefore:
+        #output = [1, batch size, dec hid dim]
+        #hidden = [1, batch size, dec hid dim]
+        #this also means that output == hidden
+        assert (output == hidden).all()
+        
+        embedded = embedded.squeeze(0)
+        output = output.squeeze(0)
+        weighted = weighted.squeeze(0)
+        
+        prediction = self.fc_out(torch.cat((output, weighted, embedded), dim = 1))
+        #prediction = [batch size, output dim]
+        
+        # masked attention is returned.
+        return prediction, hidden.squeeze(0), a.squeeze(1)
+
+
+'''
+Seq2seq: with a few changes for packed padded sequences, masking and inference.
+Trick: got the the source sentence length & 'create_mask' function is here.
+The attention at each time-step is stored in the 'attentions'.
+'''
+class Seq2Seq(nn.Module):
+    def __init__(self, encoder, decoder, src_pad_idx, device):
+        super().__init__()
+        
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_pad_idx = src_pad_idx
+        # Trick: define device here.
+        self.device = device
+    
+    def create_mask(self, src):
+        mask = (src != self.src_pad_idx).permute(1, 0)
+        return mask
+
+    def forward(self, src, src_len, trg, teacher_forcing_ratio = 0.5):
+        #src = [src len, batch size] (indexes)
+        #src_len = [batch size]
+        #trg = [trg len, batch size]
+        #teacher_forcing_ratio is probability to use teacher forcing
+        #e.g. if teacher_forcing_ratio is 0.75 we use teacher forcing 75% of the time
+
+        batch_size = src.shape[1]
+        trg_len = trg.shape[0]
+        trg_vocab_size = self.decoder.output_dim
+        
+        #tensor to store decoder outputs
+        outputs = torch.zeros(trg_len, batch_size, trg_vocab_size).to(self.device)
+
+        #encoder_outputs is all hidden states of the input sequence, back and forwards
+        #hidden is the final forward and backward hidden states, passed through a linear layer (change dimension)
+        encoder_outputs, hidden = self.encoder(src, src_len)
+                
+        #first input to the decoder is the <sos> tokens
+        input = trg[0,:]
+
+        # mask of source sequence
+        mask = self.create_mask(src)
+        #mask = [batch size, src len]
+
+        for t in range(1, trg_len):
+            
+            #insert input token embedding, previous hidden state, all encoder hidden states 
+            #  and mask
+            #receive output tensor (predictions) and new hidden state
+            output, hidden, _ = self.decoder(input, hidden, encoder_outputs, mask)
+            
+            #place predictions in a tensor holding predictions for each token
+            outputs[t] = output
+            
+            #decide if we are going to use teacher forcing or not
+            teacher_force = random.random() < teacher_forcing_ratio
+            
+            #get the highest predicted token from our predictions
+            top1 = output.argmax(1) 
+            
+            #if teacher forcing, use actual next token as next input
+            #if not, use predicted token
+            input = trg[t] if teacher_force else top1
+            
+        return outputs
+
+def train(model, iterator, optimizer, criterion, clip):
+    model.train()
+    epoch_loss = 0
+
+    for i, batch in enumerate(iterator):
+        
+        # Trick: include_lengths = True for our source field, 
+        # batch.src is now a tuple:
+        # 1. the numericalized tensor;
+        # 2. the lengths of each sentence within the batch.
+        src, src_len = batch.src
+
+        trg = batch.trg
+        
+        optimizer.zero_grad()
+        
+        output = model(src, src_len, trg)
+        #trg = [trg len, batch size]
+        #output = [trg len, batch size, output dim]
+        
+        output_dim = output.shape[-1]
+        
+        output = output[1:].view(-1, output_dim)
+        trg = trg[1:].view(-1)
+        #trg = [(trg len - 1) * batch size]
+        #output = [(trg len - 1) * batch size, output dim]
+        
+        # with pad_idx ignored in the corss-entropy
+        loss = criterion(output, trg)
+        
+        loss.backward()
+        
+        # Trick: backward then clip grad norm;
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+        
+        optimizer.step()
+        
+        epoch_loss += loss.item()
+        
+    return epoch_loss / len(iterator)
+
+def evaluate(model, iterator, criterion):
+    model.eval()
+    epoch_loss = 0
+    
+    with torch.no_grad():
+    
+        for i, batch in enumerate(iterator):
+
+            src, src_len = batch.src
+            trg = batch.trg
+
+            output = model(src, src_len, trg, 0) #turn off teacher forcing
+            
+            #trg = [trg len, batch size]
+            #output = [trg len, batch size, output dim]
+
+            output_dim = output.shape[-1]
+            
+            output = output[1:].view(-1, output_dim)
+            trg = trg[1:].view(-1)
+
+            #trg = [(trg len - 1) * batch size]
+            #output = [(trg len - 1) * batch size, output dim]
+
+            loss = criterion(output, trg)
+
+            epoch_loss += loss.item()
+        
+    return epoch_loss / len(iterator)
+
+'''
+Inference
+'''
--- four.py
+++ four.py
@@ -101,3 +101,74 @@ train_iterator, valid_iterator, test_iterator = BucketIterator.splits(
      sort_key = lambda x : len(x.src),
      device = device)
 
+INPUT_DIM = len(SRC.vocab)
+OUTPUT_DIM = len(TRG.vocab)
+ENC_EMB_DIM = 256
+DEC_EMB_DIM = 256
+ENC_HID_DIM = 512
+DEC_HID_DIM = 512
+ENC_DROPOUT = 0.5
+DEC_DROPOUT = 0.5
+SRC_PAD_IDX = SRC.vocab.stoi[SRC.pad_token]
+
+attn = Attention(ENC_HID_DIM, DEC_HID_DIM)
+enc = Encoder(INPUT_DIM, ENC_EMB_DIM, ENC_HID_DIM, DEC_HID_DIM, ENC_DROPOUT)
+dec = Decoder(OUTPUT_DIM, DEC_EMB_DIM, ENC_HID_DIM, DEC_HID_DIM, DEC_DROPOUT, attn)
+
+model = Seq2Seq(enc, dec, SRC_PAD_IDX, device).to(device)
+
+def init_weights(m):
+    for name, param in m.named_parameters():
+        if 'weight' in name:
+            nn.init.normal_(param.data, mean=0, std=0.01)
+        else:
+            nn.init.constant_(param.data, 0)
+            
+model.apply(init_weights)
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+print(f'The model has {count_parameters(model):,} trainable parameters')
+
+optimizer = optim.Adam(model.parameters())
+
+TRG_PAD_IDX = TRG.vocab.stoi[TRG.pad_token]
+
+# Trick: cross-entropy loss by ignoring the padded token.
+criterion = nn.CrossEntropyLoss(ignore_index = TRG_PAD_IDX)
+
+def epoch_time(start_time, end_time):
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+    return elapsed_mins, elapsed_secs
+
+N_EPOCHS = 10
+CLIP = 1
+
+best_valid_loss = float('inf')
+
+for epoch in range(N_EPOCHS):
+    
+    start_time = time.time()
+    
+    train_loss = train(model, train_iterator, optimizer, criterion, CLIP)
+    valid_loss = evaluate(model, valid_iterator, criterion)
+    
+    end_time = time.time()
+    
+    epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+    
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+        torch.save(model.state_dict(), 'models/tut4-model.pt')
+    
+    print(f'Epoch: {epoch+1:02} | Time: {epoch_mins}m {epoch_secs}s')
+    print(f'\tTrain Loss: {train_loss:.3f} | Train PPL: {math.exp(train_loss):7.3f}')
+    print(f'\t Val. Loss: {valid_loss:.3f} |  Val. PPL: {math.exp(valid_loss):7.3f}')
+
+
+model.load_state_dict(torch.load('models/tut4-model.pt'))
+test_loss = evaluate(model, test_iterator, criterion)
+print(f'| Test Loss: {test_loss:.3f} | Test PPL: {math.exp(test_loss):7.3f} |')
